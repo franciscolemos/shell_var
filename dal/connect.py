d14b05ed8d0f0ba8b455c804bdda5d6e8ae4afd0
cmp3lemosf <francisco.lemos@ntu.ac.uk>
2022-08-01 00:14:43 +0100

    Update connection for linux for env. var.

--- dal/connect.py
+++ dal/connect.py
@@ -1,11 +1,15 @@
 
 import configparser
 import eia
-
+import os
 class  Api():
     def __init__(self):
-        config = configparser.ConfigParser()
-        config.read_file(open('dal/credentials.cfg'))
+
         #encapsulation
-        key = config['Common']['APIKEY'] #accessible only inside the constructor
+        if os.name == 'nt': #for windows laptop version
+            config = configparser.ConfigParser()
+            config.read_file(open('dal/credentials.cfg')) #create file w/ EIA API key
+            key = config['Common']['APIKEY'] #accessible only inside the constructor
+        else:
+            key = os.environ.get('EIA_APIKEY')
         self.api = eia.API(key)
